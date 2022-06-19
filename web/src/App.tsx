54b25b1d6432ebf8ba9b0588da7b9a6ec3410cfd
MateusGPe <mateus.pereira1@aluno.ifsp.edu.br>
2022-06-19 19:58:55 -0300

    first commit

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import { ApolloProvider } from '@apollo/client'
+import { Anilist } from './components/Anilist'
+import AnilistClient from './components/Anilist/service'
+import { Widget } from './components/WidgetFeedback'
+
+export function App() {
+  return (<>
+    <ApolloProvider client={AnilistClient}>
+      <Anilist />
+    </ApolloProvider>
+    <Widget />
+  </>)
+}
+
---
+++ web/tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  content: ['./src/**/*.tsx'],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          300: '#005099',
+          500: '#22C55E',
+        },
+        gray: {
+          900: '#202225',
+          800: '#2f3136',
+          700: '#36393f',
+          600: '#4f545c',
+          400: '#d4d7dc',
+          300: '#e3e5e8',
+          200: '#ebedef',
+          100: '#f2f3f5',
+        },
+      },
+      borderRadius: {
+        md: '4px',
+      }
+    },
+  },
+  plugins: [
+    require('tailwind-scrollbar'),
+    require('@tailwindcss/forms'),
+  ],
+}
